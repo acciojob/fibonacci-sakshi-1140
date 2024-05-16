f17d8a810e948cce5a536373d20cc101c7988280
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-05-16 19:44:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function fibonacci(num) {
 // your code here
-	let ans
-	if(num=1) ans=0
-	else if(num=2) ans=1
-	else ans=fibonacci(num-1)+fibonacci(num-2)
+	//let ans
+	if(num=1) return 0
+	if(num=2) return 1
+	else{
+		let ans=fibonacci(num-1)+fibonacci(num-2)
 	return ans
+	}
 }
 
 module.exports = fibonacci;
