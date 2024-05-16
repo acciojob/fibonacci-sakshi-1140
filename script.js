6608c9488fb95d26d73c9a740fbced0a6d93cd47
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-05-16 19:58:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,18 @@ function fibonacci(num) {
 	//let ans
 	if(num==1) return 0
 	if(num==2) return 1
+		
 	else{
-		let ans=fibonacci(num-1)+fibonacci(num-2)
-	return ans
+		//let ans=fibonacci(num-1)+fibonacci(num-2)
+		let num1=0
+		let num2=1
+		let sum=0
+		for(let i=2;i<=num;i++){
+			sum=num2+num1;
+			num1=num2;
+			num2=sum
+		}
+	return sum
 	}
 }
 
